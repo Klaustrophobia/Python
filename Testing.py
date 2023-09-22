6111b37e251fb5b6705263ed6dd5dc5c3db495f9
Kevin Josue Guzman Zelaya <70920346+Klaustrophobia@users.noreply.github.com>
2023-09-21 19:00:20 -0600

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,16 @@
+"""
+Las funciones deben estar arriba recordando que al ser lenguaje interpretado
+lee linea por linea.
+"""
+
+def imprimir (parametro):
+    print(parametro)
+
+print("Hola Mundo")
+## Comando "Es para imprimir en la CMD"
+
+imprimir("Impresion II")
+
+## Input recibe datos del usuario
+input(" Label que va salir en la CMD ")
+
